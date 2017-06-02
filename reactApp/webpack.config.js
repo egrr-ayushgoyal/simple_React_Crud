91ccbfd6f0fc007d898f8e19e7e322bdfbb738c1
egrr-ayushgoyal <egrr.ayushgoyal@gmail.com>
2017-06-02 19:10:27 +0530

    Add files via upload
    React configuration with webpack as on tutorals point.

---
+++ reactApp/webpack.config.js
@@ -0,0 +1,51 @@
+/*
+var config = {
+   entry: './main.js',
+	
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;*/
+
+
+var path = require('path');
+
+module.exports = {
+    entry: "./app/app.js",
+    output: {
+        filename: "public/js/bundle.js",
+        sourceMapFilename: "public/js/bundle.map"
+    },
+    devtool: '#source-map',
+    module: {
+        loaders: [
+            {
+
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+
